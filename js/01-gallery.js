d956eec4cd3d8cbbdd33dd7930b63bae2baf5409
unknown <gudymavit1988@gmail.com>
2022-03-08 12:12:18 +0200

    hw-07

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from './gallery-items.js';
+
+
+const galleryConteiner = document.querySelector('.gallery');
+const ImagesCard = createImagesCard(galleryItems);
+
+
+galleryConteiner.insertAdjacentHTML('beforeend', ImagesCard);
+
+galleryConteiner.addEventListener('click', onGalleryConteinerClick)
+
+
+function createImagesCard(images) {
+    return images.map(({ description, original, preview }) => {
+      return`<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src= ${preview}
+          data-source= ${original}
+          alt= ${description}
+        />
+      </a>
+    </div>`
+}).join('');
+};
+
+
+function onGalleryConteinerClick(event) {
+    event.preventDefault();
+    const isGalleryImageEl = event.target.classList.contains('gallery__image');
+    if (!isGalleryImageEl){
+        return;
+    }
+  
+  const imagaEl = event.target.dataset.source;
+  
+    const modal = basicLightbox.create(`<img src="${imagaEl}">`, {
+    onShow: () => {
+      window.addEventListener('keydown',onKeyPress);
+    },
+    onClose:()=>{
+      window.addEventListener('keydown', onKeyPress)
+    }
+  })
+
+  function onKeyPress(e) {
+    
+    if (e.code === 'Escape') {
+      modal.close();
+    }
+  }
+  modal.show();
+};    
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+
+
+const galleryConteiner = document.querySelector('.gallery');
+const ImagesCard = createImagesCard(galleryItems);
+console.log(ImagesCard);
+
+galleryConteiner.insertAdjacentHTML('beforeend', ImagesCard);
+
+galleryConteiner.addEventListener('click', onGalleryConteinerClick)
+
+const gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+
+function createImagesCard(images) {
+    return images.map(({ description, original, preview }) => {
+      return `
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src= ${preview}
+        data-source= ${original}
+        alt= ${description} 
+      />
+  </a>`
+}).join('');
+};
+
+
+function onGalleryConteinerClick(event) {
+    event.preventDefault();
+        if (event.target.nodeName !== 'IMG'){
+        return gallery;
+    }    
+}; 
